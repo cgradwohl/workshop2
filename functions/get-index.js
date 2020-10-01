997d356d18bdd6ee8961e799db923d2c3b58a13e
Chris Gradwohl <christophergradwohl@gmail.com>
2020-10-01 16:29:45 -0700

    securing API with IAM auth.

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,30 +1,37 @@
 const fs = require("fs")
 const Mustache = require('mustache')
 const http = require('axios')
+const aws4 = require('aws4')
+const URL = require('url')
 
 const restaurantsApiRoot = process.env.restaurants_api
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-let html
-
-function loadHtml () {
-  if (!html) {
-    console.log('loading index.html...')
-    html = fs.readFileSync('static/index.html', 'utf-8')
-    console.log('loaded')
-  }
-  
-  return html
-}
+const template = fs.readFileSync('static/index.html', 'utf-8')
 
 const getRestaurants = async () => {
-  const httpReq = http.get(restaurantsApiRoot)
+  console.log(`loading restaurants from ${restaurantsApiRoot}...`)
+  const url = URL.parse(restaurantsApiRoot) // restaurantsApiRoot --> https://#{ApiGatewayRestApi}.execute-api.#{AWS::Region}.amazonaws.com/${self:provider.stage}/restaurants
+  const opts = {
+    host: url.hostname,
+    path: url.pathname
+  }
+
+  // sign the request to get past the aws_iam authorizer
+  aws4.sign(opts)
+
+  // NOTE:#1
+  // HERE WE ARE MAKING A REQUEST TO A LAMBDA FROM THIS LAMBDA
+  // this is an example of making a request to a private/ internal API that requires an IAM POLICY to pass the aws_iam authorizer
+  const httpReq = http.get(restaurantsApiRoot, {
+    headers: opts.headers
+  })
   return (await httpReq).data
 }
 
 module.exports.handler = async (event, context) => {
-  const template = loadHtml()
   const restaurants = await getRestaurants()
+  console.log(`found ${restaurants.length} restaurants`)  
   const dayOfWeek = days[new Date().getDay()]
   const html = Mustache.render(template, { dayOfWeek, restaurants })
   const response = {
